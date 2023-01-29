c6c1f83fba5b772619bfd2c7a9f5d6a498d8f8e1
Alex <Alex@HOME-PC>
2023-01-29 23:34:00 +0300

    Change

---
+++ Task 13.6.2/Task 13.6.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Linq;
+
+namespace Task_13._6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:\\Users\\Alex\\Desktop\\Text1.txt");
+            var a = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            string[] words = a.Split(new char[] { ' ' });
+            var words1 = new Dictionary<string, int>();
+
+            foreach (var i in Distinct(words))
+            words1.Add(i, DigitCount(words, i));
+
+            List<KeyValuePair<string, int>> mappings = words1.ToList();
+            mappings.Sort((x, y) => x.Value.CompareTo(y.Value));
+
+            int j = words.Length;
+            for (int i= j; i == j - 10; i=i-1)
+            { Console.WriteLine(mappings);}
+            
+
+           
+        }
+        static string[] Distinct(string[] a)
+            {
+                HashSet<string> list = new HashSet<string>(a);
+                string[] b = new string[list.Count];
+                list.CopyTo(b);
+                return b;
+            }
+        static int DigitCount(string[] a, string b)
+        {
+            int count = 0;
+
+            foreach (var i in a)
+                if (i == b) count++;
+
+            return count;
+        }
+    }
+}
---
+++ Task13.6.1/Task13.6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace Task13._6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //создали список text, присвоили строку из файла
+            List<string> text = new List<string>() { File.ReadAllText("C:\\Users\\Alex\\Desktop\\Text.txt") };
+            // запустили таймер
+            var watchTwo = Stopwatch.StartNew();
+            // добавили новый элемент из файла
+            text.Add("Tom");
+            // удалили новый элемент из файла
+            text.Remove("Tom");
+            //Выведем результат
+            Console.WriteLine($"Вставка в коллекцию List<T>: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+
+            //------------------------------------------------
+            var text1 = new LinkedList<string> (text);
+           // var watchTwo = Stopwatch.StartNew();
+            text1.AddLast("Tom");
+            text1.RemoveLast();
+            Console.WriteLine($"Вставка в коллекцию LinkedList<T>: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+        }
+    }
+}
